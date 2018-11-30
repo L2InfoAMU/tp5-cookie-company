bb08c43b136dbaa154ea55afa428234722f6f06f
Wery Alexandre <alexandre.wery@etu.univ-amu.fr>
2018-11-30 09:53:39 +0100

    code refactor:  - BruteRasterImage  - PaletteRasterImage into RasterImage

--- src/main/java/image/BruteRasterImage.java
+++ src/main/java/image/BruteRasterImage.java
@@ -1,90 +1,28 @@
 package image;
 
 import javafx.scene.paint.Color;
-import util.Matrices;
 
-import java.util.Arrays;
-
-public class BruteRasterImage implements Image {
-    private int width;
-    private int height;
-    private Color[][] pixels;
-
+public class BruteRasterImage extends RasterImage<Color> {
     public BruteRasterImage(Color color, int width, int height) {
-        this.width = width;
-        this.height = height;
+        setWidth(width);
+        setHeight(height);
+
         createRepresentation();
-        for(Color[] lines : pixels)
-            Arrays.fill(lines, color);
+
+        fill(color);
     }
 
     public BruteRasterImage(Color[][] colors) {
         setPixelsColor(colors);
     }
 
-    public void createRepresentation(){
-        assert width>0;
-        assert height>0;
-
-        pixels = new Color[height][width];
-    }
-
-    public void setPixelColor(int x, int y, Color color) {
-        if(x >= width || y >= height || x < 0 || y < 0)
-            throw new IndexOutOfBoundsException("pixel not inside picture");
-
-        pixels[y][x] = color;
-    }
-
-    public void setPixelsColor(Color[][] colors) {
-        Matrices.requiresNonNull(colors);
-        Matrices.requiresNonZeroDimensions(colors);
-        Matrices.requiresRectangularMatrix(colors);
-
-        pixels = colors;
-        height = pixels[0].length;
-        width = pixels.length;
-        pixels = new Color[height][width];
-
-        for (int y = 0; y < height; y++) {
-            for (int x = 0; x < width; x++) {
-                pixels[y][x] = colors[x][y];
-            }
-        }
+    @Override
+    public void setPixelColor(Color color, int x, int y) {
+        setRawPixelColor(color, x, y);
     }
 
     @Override
     public Color getPixelColor(int x, int y) {
-        if(x >= width || y >= height || x < 0 || y < 0)
-            throw new IndexOutOfBoundsException("pixel not inside picture");
-
-        return pixels[y][x];
-    }
-
-    @Override
-    public int getWidth() {
-        return width;
-    }
-
-    @Override
-    public int getHeight() {
-        return height;
-    }
-
-    protected void setWidth(int width) {
-        this.width = width;
-
-        for (int y = 0; y < height; y++) {
-            if(pixels[y] == null)
-                pixels[y] = new Color[width];
-            else
-                pixels[y] = Arrays.copyOf(pixels[y], width);
-        }
-    }
-
-    protected void setHeight(int height) {
-        this.height = height;
-
-        pixels = Arrays.copyOf(pixels, height);
+        return getRawPixelColor(x, y);
     }
 }
---
+++ src/main/java/image/RasterImage.java
@@ -0,0 +1,95 @@
+package image;
+
+import javafx.scene.paint.Color;
+import util.Matrices;
+
+import java.util.Arrays;
+
+public abstract class RasterImage<T> implements Image {
+    protected int width;
+    protected int height;
+    protected T[][] pixelColors;
+
+
+    public void createRepresentation(){
+        if(width<0 || height<0)
+            throw new IndexOutOfBoundsException("invalid picture size");
+
+        pixelColors = (T[][]) new Object[height][width];
+    }
+
+    protected void setRawPixelColors(T[][] pixelColors) {
+        this.pixelColors = pixelColors;
+    }
+
+    protected void setRawPixelColor(T color, int x, int y) {
+        pixelColors[y][x] = color;
+    }
+
+    protected T getRawPixelColor(int x, int y) {
+        return pixelColors[y][x];
+    }
+
+    public abstract void setPixelColor(Color color, int x, int y);
+
+    public void setPixelsColor(Color[][] colors) {
+        checkPictureBound(colors);
+
+        setHeight(colors[0].length);
+        setWidth(colors.length);
+
+        createRepresentation();
+
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                setPixelColor(colors[x][y], x, y);
+            }
+        }
+    }
+
+    @Override
+    public int getWidth() {
+        return width;
+    }
+
+    @Override
+    public int getHeight() {
+        return height;
+    }
+
+    protected void applyResize() {
+        pixelColors = Arrays.copyOf(pixelColors, height);
+
+        for (int i = 0; i < height; i++) {
+            if(pixelColors[i] == null)
+                pixelColors[i] = (T[]) new Object[this.width];
+            else
+                pixelColors[i] = Arrays.copyOf(pixelColors[i], this.width);
+        }
+    }
+
+    protected void setWidth(int width) {
+        this.width = width;
+
+    }
+
+    protected void setHeight(int height) {
+        this.height = height;
+    }
+
+    protected void checkPixelBound(int x, int y) {
+        if(x >= width || y >= height || x < 0 || y < 0)
+            throw new ArrayIndexOutOfBoundsException("pixel not inside picture");
+    }
+
+    protected void checkPictureBound(Color[][] colors) {
+        Matrices.requiresNonNull(colors);
+        Matrices.requiresNonZeroDimensions(colors);
+        Matrices.requiresRectangularMatrix(colors);
+    }
+
+    protected void fill(T color) {
+        for(T[] lines : pixelColors)
+            Arrays.fill(lines, color);
+    }
+}
